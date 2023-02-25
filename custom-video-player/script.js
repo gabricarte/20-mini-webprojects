676f6e99403458cdade175dacccfee6249640208
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2023-02-25 11:20:23 -0300

    video player add

---
+++ custom-video-player/script.js
@@ -0,0 +1,82 @@
+//Using HTML Video DOM Reference
+
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress'); //progress bar
+const timestamp = document.getElementById('timestamp');
+
+//Event Listeners
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
+
+
+
+//Functions
+
+//Play and pause video
+function toggleVideoStatus(){
+
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();  
+    }
+
+}
+
+
+//Update the icons according to status of the video
+function updatePlayIcon(){
+    
+    if(video.paused){
+        play.innerHTML = '<i class="fa fa-play fa-2x"> </i>';
+    } else{
+        play.innerHTML = '<i class="fa fa-pause fa-2x"> </i>';
+    }
+}
+
+//Update the video progress and the timestamp
+function updateProgress(){
+    //console.log(video.currentTime); - You can see the seconds that are getting played
+    progress.value = (video.currentTime / video.duration) * 100;   //making the progress bar move according to duration percentage
+
+
+     //timestamp
+
+     //Get Minutes
+     let mins = Math.floor(video.currentTime / 60);
+
+     if (mins < 10){
+        mins = '0' + String(mins);
+     }
+
+
+     //Get Seconds 
+     let secs = Math.floor(video.currentTime % 60);
+
+     if (secs < 10){
+        secs = '0' + String(secs);
+     }
+
+     timestamp.innerHTML = `${mins}:${secs}`
+
+}
+
+
+function stopVideo(){
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+function setVideoProgress(){
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
